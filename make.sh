175ae1715d49d43697b0dc1f982766d5ac57e97a
TheMrcool212 <munirmohamadrani@gmail.com>
2014-03-25 15:37:51 +0800

    Add a simple build script

---
+++ make.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exports all the needed things Arch, SubArch and Cross Compile
+export ARCH=arm
+echo 'exporting Arch'
+export SUBARCH=arm
+echo 'exporting SubArch'
+export CROSS_COMPILE=/home/aospal/n7/prebuilts/gcc/inux-x86/arm/arm-eabi-4.9/bin/arm-eabi-
+echo 'exporting Cross Compile'
+
+# Make sure build is clean!
+echo 'Cleaning build'
+make clean
+
+# Generates a new .config and exists
+if [ "$1" = "config" ] ; then
+echo 'Making defconfig for Flo'
+make aospal_flo_defconfig
+exit
+fi
+
+# Build it
+echo 'Lets start!'
+make -j$1
